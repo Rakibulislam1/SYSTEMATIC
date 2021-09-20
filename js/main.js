5da27ca54f607945152955f9795a2debc900ab95
Rakibul Islam <81636405+Rakibulislam1@users.noreply.github.com>
2021-09-20 21:43:24 +0600

    SYSTEMATIC project

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// bxSlider
+
+$(document).ready(function () {
+    $('.slider_area').bxSlider();
+    jQuery('#mian_menu').meanmenu({
+        meanMenuContainer: '#mobile_menu',
+        meanScreenWidth: 991,
+    });
+});
